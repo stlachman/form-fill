9c957259f19d47cb91547b4b9f838805bf179324
Seth Lachman <lachmanseth17@gmail.com>
2019-07-15 14:36:21 -0400

    Updated form.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+// this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 import "./App.css";
 
 class App extends React.Component {
@@ -8,20 +11,36 @@ class App extends React.Component {
     this.state = {
       name: "",
       section: "",
-      students: []
+      students: [],
+      student: ""
     };
   }
 
   render() {
-    const { name, section } = this.state;
+    const { name, section, student } = this.state;
     return (
-      <section>
+      <section
+        css={css`
+          margin: 0 auto;
+          max-width: 300px;
+        `}
+      >
         <h1>Attendance</h1>
         <div>
           <form>
             <div>
-              <label htmlFor="name">Your Name</label>
+              <label
+                css={css`
+                  display: block;
+                `}
+                htmlFor="name"
+              >
+                Your Name
+              </label>
               <input
+                css={css`
+                  margin-bottom: 10px;
+                `}
                 name="name"
                 id="name"
                 placeholder="Captain Marvel"
@@ -31,8 +50,18 @@ class App extends React.Component {
               />
             </div>
             <div>
-              <label htmlFor="section">Section</label>
+              <label
+                css={css`
+                  display: block;
+                `}
+                htmlFor="section"
+              >
+                Section
+              </label>
               <input
+                css={css`
+                  margin-bottom: 10px;
+                `}
                 name="section"
                 id="section"
                 placeholder="WEB22"
@@ -40,6 +69,35 @@ class App extends React.Component {
                 value={section.value}
                 onChange={this.handleChange}
               />
+            </div>
+            <div>
+              <label
+                css={css`
+                  display: block;
+                `}
+                htmlFor="student"
+              >
+                Student Name
+              </label>
+              {this.state.students.map((student, index) => {
+                return (
+                  <div>
+                    <input
+                      onChange={this.handleChange}
+                      key={index}
+                      type="text"
+                      name="student"
+                      value={student}
+                    />
+                  </div>
+                );
+              })}
+              <button type="button" onClick={this.addStudent}>
+                Add Student
+              </button>
+            </div>
+            <div>
+              <button>Store Values</button>
             </div>
           </form>
         </div>
@@ -51,6 +109,15 @@ class App extends React.Component {
     this.setState({
       [event.target.id]: event.target.value
     });
+  };
+
+  addStudent = event => {
+    this.setState({ students: [...this.state.students, ""] });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { student, students } = this.state;
   };
 }
 
